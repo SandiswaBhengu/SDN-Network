6df055fe7a3f7c8c1bf1523cf8f4333cb7e9599d
SandiswaBhengu <sandiswa@mbvit.co.za>
2026-01-10 21:57:05 +0200

    Update stp.sh

--- ovs/stp.sh
+++ ovs/stp.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+BRIDGE="br0"
+
+echo "Enabling STP on bridge $BRIDGE..."
+
+# Enable STP on the bridge
+ovs-vsctl set bridge "$BRIDGE" stp_enable=true
+
+echo "STP enabled on $BRIDGE"
+
+echo "Verifying STP status..."
+ovs-vsctl list bridge "$BRIDGE" | grep stp
